a36f0132ebb60bda570f15cff30404fd207a97c0
pawzwo <pawelzwolinski@poczta.onet.pl>
2021-11-15 20:17:38 +0100

    Admins Class for testing of git

---
+++ src/main/java/pl/coderslab/model/Admins.java
@@ -0,0 +1,89 @@
+package pl.coderslab.model;
+
+public class Admins {
+
+    private int id = 0;
+    private String firstName = "firstName";
+    private String lastName = "lastName";
+    private String email = "email";
+    private String password = "password";
+    private int superadmin = 0;
+    private int enable = 0;
+
+    public Admins(String firstName, String lastName, String email, String password, int superadmin, int enable) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.superadmin = superadmin;
+        this.enable = enable;
+    }
+
+    public Admins() {
+    }
+
+    @Override
+    public String toString() {
+        return "Admins{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", superadmin=" + superadmin +
+                ", enable=" + enable +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSuperadmin() {
+        return superadmin;
+    }
+
+    public void setSuperadmin(int superadmin) {
+        this.superadmin = superadmin;
+    }
+
+    public int getEnable() {
+        return enable;
+    }
+
+    public void setEnable(int enable) {
+        this.enable = enable;
+    }
+}

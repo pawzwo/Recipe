8c7e2246b9ad444297d8df409ce50961d8a7f275
elkuchta <lukasz.kuchta.90@gmail.com>
2021-11-15 17:49:39 +0100

    PlanDao

---
+++ src/main/java/pl/coderslab/model/Plan.java
@@ -0,0 +1,59 @@
+package pl.coderslab.model;
+
+public class Plan {
+
+    private int id;
+    private String name;
+    private String description;
+    private String created;
+    private int adminId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+   public Plan(String name, String description, String created){
+        this.name=name;
+        this.description=description;
+        this.created=created;
+   }
+   public Plan(){
+
+   }
+}

2b69bc7bafd1beba1492d0d6d954119db16118f4
pawzwo <pawelzwolinski@poczta.onet.pl>
2021-11-16 16:54:41 +0100

    Dodany LandingPageServlet z adresem "" oraz do wyswietlenia landingPage.jsp, ktory odpowiada index.html W HomeServlet zmieniony adres, tak żeby nie wchodzil w konflikt LandingPageServlet

---
+++ src/main/java/pl/coderslab/web/LandingPageServlet.java
@@ -0,0 +1,24 @@
+package pl.coderslab.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("")
+public class LandingPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        getServletContext().getRequestDispatcher("/jsp/landingPage.jsp").forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}

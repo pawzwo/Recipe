1aafdd904abcdbfa4258f681c7c69c1e0b34ed53
Piotr Adamczyk <piotr.adamczyk89@gmail.com>
2021-11-17 00:02:15 +0100

    filtr do logowania

---
+++ src/main/java/pl/coderslab/filtr/loginFilter.java
@@ -0,0 +1,28 @@
+package pl.coderslab.filtr;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/app/*")
+public class loginFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request1 = (HttpServletRequest) request;
+        HttpServletResponse response1 = (HttpServletResponse) response;
+        HttpSession session= request1.getSession();
+        if (session.getAttribute("login")==null) {
+            response1.sendRedirect("/login");
+        }
+        chain.doFilter(request, response);
+    }
+}

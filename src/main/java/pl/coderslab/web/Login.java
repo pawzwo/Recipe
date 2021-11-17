0719436fb85bf3b30cf2959521c459a723670a52
pawzwo <pawelzwolinski@poczta.onet.pl>
2021-11-18 00:47:14 +0100

    dorobiony post login i login.jsp uzupelniony o komunikaty

--- src/main/java/pl/coderslab/web/Login.java
+++ src/main/java/pl/coderslab/web/Login.java
@@ -1,5 +1,7 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -10,7 +12,7 @@ public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.getRequestDispatcher("login.jsp").forward(request, response);
+        request.getRequestDispatcher("jsp/login.jsp").forward(request, response);
 
 
     }
@@ -22,9 +24,29 @@ public class Login extends HttpServlet {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
+        AdminDao adminDao = new AdminDao();
+        if (email != null && password != null && !email.isBlank() && !password.isBlank() && !email.isEmpty() && !password.isEmpty()) {
+            System.out.println("step1");
+            if (adminDao.verifyEmail(email)) {
+                System.out.println("step2");
+                if (adminDao.verifyPassword(email, password)) {
+                    int adminId = adminDao.readByEmail(email).getId();
+                    int superadmin = adminDao.readByEmail(email).getSuperAdmin();
+                    System.out.println(adminId);
+                    System.out.println(superadmin);
+                    HttpSession session = request.getSession();
+                    session.setAttribute("id", adminId);
+                    session.setAttribute("superadmin", superadmin);
+                    session.setMaxInactiveInterval(3600);
+                    response.sendRedirect("/jsp/dashboard.jsp");
+                } else {
+                    response.sendRedirect("/login?password=0");
+                }
+            } else {
+                response.sendRedirect("/login?email=0");
+            }
+        }
 
 
-        response.getWriter().append("Witaj ").append(email);}//<podane imię> <podane nazwisko>.")
-
-
+    }
 }
